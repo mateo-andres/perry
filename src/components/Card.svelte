6483e142b5263353cc26c82c18d3e888099c017b
takechi <aniampira@gmail.com>
2023-12-04 20:34:44 -0500

    Add Card component and update home layout

---
+++ src/components/Card.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let title;
+	export let button = false;
+</script>
+
+<div class="card bg-slate-400 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title"><slot name="icon" /> {title}</h2>
+		<slot />
+		{#if button}
+			<div class="card-actions justify-center">
+				<button class="btn btn-primary">{button}</button>
+			</div>
+		{/if}
+	</div>
+</div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,33 +1,61 @@
 <script>
+	import Card from '$components/Card.svelte';
 </script>
 
-<h1>Inicio</h1>
+<main class="p-6 grid gap-y-5 mb-16">
+	<h1 class="font-bold text-3xl text-center">Inicio</h1>
 
-<button class="btn btn-outline">Lista pendientes</button>
-<button class="btn btn-outline btn-primary">Recomendaciones</button>
-<button class="btn btn-outline btn-secondary">Notas</button>
-<button class="btn btn-outline btn-accent">Calendario</button>
+	<Card title={'Pendientes'} button={'Agregar tarea'}>
+		<i slot="icon" class="fa-solid fa-list-check" />
+		<label class="cursor-pointer label justify-start gap-4">
+			<input type="checkbox" checked="checked" class="checkbox checkbox-accent" />
+			<span class="label-text">Tarea 1</span>
+		</label>
 
-<h2 class="font-bold text-2xl text-center mt-8">Notas</h2>
-<div class="flex flex-col items-center justify-center gap-4">
-	<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
-		<h3 class="font-bold text-xl">Matemáticas</h3>
-		<p class="text-gray-500">4.5</p>
-	</div>
-	<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
-		<h3 class="font-bold text-xl">Español</h3>
-		<p class="text-gray-500">4.5</p>
-	</div>
-	<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
-		<h3 class="font-bold text-xl">Ciencias</h3>
-		<p class="text-gray-500">4.5</p>
-	</div>
-	<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
-		<h3 class="font-bold text-xl">Sociales</h3>
-		<p class="text-gray-500">4.5</p>
-	</div>
-	<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
-		<h3 class="font-bold text-xl">Inglés</h3>
-		<p class="text-gray-500">4.5</p>
-	</div>
-</div>
+		<label class="cursor-pointer label justify-start gap-4">
+			<input type="checkbox" checked="checked" class="checkbox checkbox-accent" />
+			<span class="label-text">Tarea 2</span>
+		</label>
+
+		<label class="cursor-pointer label justify-start gap-4">
+			<input type="checkbox" checked="checked" class="checkbox checkbox-accent" />
+			<span class="label-text">Tarea 3</span>
+		</label>
+	</Card>
+
+	<Card title={'Recomendaciones'} button={'Ir'}>
+		<p>Descubir Recomendaciones y ver historico</p>
+	</Card>
+
+	<Card title={'Notas'} button={'Ver notas'}>
+		<i slot="icon" class="fa-solid fa-book" />
+
+		<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
+			<h3 class="font-bold text-xl">Matemáticas</h3>
+			<p class="text-gray-500">4.5</p>
+		</div>
+		<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
+			<h3 class="font-bold text-xl">Español</h3>
+			<p class="text-gray-500">4.5</p>
+		</div>
+		<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
+			<h3 class="font-bold text-xl">Ciencias</h3>
+			<p class="text-gray-500">4.5</p>
+		</div>
+		<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
+			<h3 class="font-bold text-xl">Sociales</h3>
+			<p class="text-gray-500">4.5</p>
+		</div>
+		<div class="flex flex-col items-center justify-center bg-orange-100 rounded-xl p-4">
+			<h3 class="font-bold text-xl">Inglés</h3>
+			<p class="text-gray-500">4.5</p>
+		</div>
+	</Card>
+
+	<Card title={'Calendario'} button={'Agregar'}>
+		<i slot="icon" class="fa-solid fa-calendar-days" />
+		<Card title={'Evento 1'} />
+		<Card title={'Evento 1'} />
+		<Card title={'Evento 1'} />
+	</Card>
+</main>
