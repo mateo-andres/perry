fc7a568d8ef9592260b1179e51a75509f5972132
takechi <aniampira@gmail.com>
2023-12-15 19:22:38 -0500

    fix footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,18 @@
-<div class="btm-nav">
-	<a class="text-orange-700" href="/home">
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="btm-nav btm-nav-xs">
+	<a class="text-orange-700 {$page.url.pathname === '/home' ? 'active' : ''}" href="/home">
 		<i class="fa-solid fa-house" />
 	</a>
-	<a class="text-orange-700 active" href="/assistant">
+	<a
+		class="text-orange-700 {$page.url.pathname === '/assistant' ? 'active' : ''}"
+		href="/assistant"
+	>
 		<i class="fa-solid fa-robot" />
 	</a>
-	<a class="text-orange-700" href="/user">
+	<a class="text-orange-700 {$page.url.pathname === '/user' ? 'active' : ''}" href="/user">
 		<i class="fa-solid fa-user-astronaut" />
 	</a>
 </div>
